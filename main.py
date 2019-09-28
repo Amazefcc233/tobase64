8cce22289fe911f60271d636c6b75bf4aaba8e31
Amazefcc233 <34246558+Amazefcc233@users.noreply.github.com>
2019-09-28 12:37:32 +0800

    我也不知道为什么会重复出现两次相同代码

--- main.py
+++ main.py
@@ -2,105 +2,44 @@ import base64
 import time
 
 def ToBase64(file, txt):
-    with open(file, 'rb') as fileObj:
-        image_data = fileObj.read()
-        base64_data = base64.b64encode(image_data)
-        fout = open(txt, 'w')
-        fout.write(base64_data.decode())
-        fout.close()
-        print("Complete!")
+	with open(file, 'rb') as fileObj:
+		image_data = fileObj.read()
+		base64_data = base64.b64encode(image_data)
+		fout = open(txt, 'w')
+		fout.write(base64_data.decode())
+		fout.close()
+		print("Complete!")
 
 
 def ToFile(txt, file):
-    with open(txt, 'r') as fileObj:
-        base64_data = fileObj.read()
-        file_info = base64_data[0:5]
-        if file_info == "data:":
-            print("Err: base64文件格式错误：不应存在data:头")
-            return True
-        else:
-            ori_image_data = base64.b64decode(base64_data)
-            fout = open(file, 'wb')
-            fout.write(ori_image_data)
-            fout.close()
-            print("Complete!")
-            return False
+	with open(txt, 'r') as fileObj:
+		base64_data = fileObj.read()
+		file_info = base64_data[0:5]
+		if file_info == "data:":
+			print("Err: base64文件格式错误：不应存在data:头")
+			return True
+		else:
+			ori_image_data = base64.b64decode(base64_data)
+			fout = open(file, 'wb')
+			fout.write(ori_image_data)
+			fout.close()
+			print("Complete!")
+			return False
+
 
 go = True
 while go:
 	filename = input("请输入欲转换文件名：")
-#	filebase = eval(input("请输入转换类型（1为到base64，2为到二进制，其余数字无效）："))
-	input_suffix = filename[-3:]
-	
-#	if filebase == 1:
-	if input_suffix == "txt":
-		print("您当前正要执行[base64转二进制]，是否执行？")
-		confirm = eval(input("（输入1执行，其他内容无效）："))
-		if confirm == 1:
-			file_suffix_name = input("请输入转换后文件后缀名：")
-			go = ToFile(f"./{filename}",f'{filename}_decode.{file_suffix_name}')  # base64编码转换为二进制文件
-			time.sleep(5)
-		else: 
-			print("无效输入，请重新输入")
+	filebase = eval(input("请输入转换类型（1为到base64，2为到二进制，其余数字无效）："))
+#	input_suffix = filename[-3:]  # 读取文件名最后三位
+
+	if filebase == 2:
+		file_suffix_name = input("请输入转换后文件后缀名：")
+		go = ToFile(f"./{filename}", f'{filename}_decode.{file_suffix_name}')  # base64编码转换为二进制文件
+		time.sleep(5)
+	elif filebase == 1: 
+		ToBase64(f"./{filename}", f'{filename}_base64.txt')  # 文件转换为base64
+		go = False
+		time.sleep(5)
 	else:
-		print("您当前正要执行[二进制转base64]，是否执行？")
-		confirm = eval(input("（输入1执行，其他内容无效）："))
-		if confirm == 1:
-			ToBase64(f"./{filename}",f'{filename}_base64.txt')  # 文件转换为base64import base64
-import time
-
-def ToBase64(file, txt):
-    with open(file, 'rb') as fileObj:
-        image_data = fileObj.read()
-        base64_data = base64.b64encode(image_data)
-        fout = open(txt, 'w')
-        fout.write(base64_data.decode())
-        fout.close()
-        print("Complete!")
-
-
-def ToFile(txt, file):
-    with open(txt, 'r') as fileObj:
-        base64_data = fileObj.read()
-        file_info = base64_data[0:5]
-        if file_info == "data:":
-            print("Err: base64文件格式错误：不应存在data:头")
-            return True
-        else:
-            ori_image_data = base64.b64decode(base64_data)
-            fout = open(file, 'wb')
-            fout.write(ori_image_data)
-            fout.close()
-            print("Complete!")
-            return False
-
-go = True
-while go:
-	filename = input("请输入欲转换文件名：")
-#	filebase = eval(input("请输入转换类型（1为到base64，2为到二进制，其余数字无效）："))
-	input_suffix = filename[-3:] # 读取文件名最后三位
-	
-#	if filebase == 1:
-	if input_suffix == "txt": # 判断文件名最后三位是否为txt
-		print("您当前正要执行[base64转二进制]，是否执行？")
-		confirm = input("（输入1执行，其他内容无效）：")
-		if confirm == '1':
-			file_suffix_name = input("请输入转换后文件后缀名：")
-			go = ToFile(f"./{filename}",f'{filename}_decode.{file_suffix_name}')  # base64编码转换为二进制文件
-			time.sleep(5)
-		else: 
-			print("无效输入，请重新输入")
-	else:
-		print("您当前正要执行[二进制转base64]，是否执行？")
-		confirm = input("（输入1执行，其他内容无效）：")
-		if confirm == '1':
-			ToBase64(f"./{filename}",f'{filename}_base64.txt')  # 文件转换为base64
-			go = False
-			time.sleep(5)
-		else: 
-			print("无效输入，请重新输入")
-
-			go = False
-			time.sleep(5)
-		else: 
-			print("无效输入，请重新输入")
+		print("无效输入，请重新输入")
